e9b100964449c9ca282d9256c47ce88dbeb643f1
DanielOliveira3 <danielbarretech@gmail.com>
2025-04-01 10:32:23 -0300

    homealuno_V0.1

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ def login():
 @app.route("/cadastrarse")
 def cadastrarse():
     return render_template("cadastrarse.html")
+
+@app.route("/aluno")
+def aluno():
+    return render_template("homealuno.html")
+
+@app.route("/nutri")
+def nutri():
+    return render_template("homenutri.html")
+
+@app.route("/cardapio")
+def cardapio():
+    return render_template("")
