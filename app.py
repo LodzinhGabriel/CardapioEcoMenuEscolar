80930ebcc322c2e34b3b6c6bec732efb79cd2088
LodzinhGabriel <gabrielgamerroblox@gmail.com>
2025-10-06 14:00:51 -0300

    sqlalchemy + revisão pag inicial

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
 from flask import (Flask, render_template, request)
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'meu_segredo_super_seguro'  # Usado para sessão
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Caminho do banco
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
+db = SQLAlchemy(app)
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    tipo = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Meta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ano = db.Column(db.Integer, nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+
+class Calendario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime, nullable=False)
+    anexo = db.Column(db.String(100), nullable=False)
+
+class Desperdicio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float, nullable=False)
+    qtdAlunos = db.Column(db.Integer, nullable=False)
+    sobras = db.Column(db.Float, nullable=False)
+    comidaFeita = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=['GET'])
 def pagina_incial():
     return render_template("paginainicial.html")
 
@@ -33,3 +65,4 @@ def calendario():
 @app.route("/sobre")
 def sobre():
     return render_template("sobre.html")
+
